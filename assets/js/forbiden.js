fa3520d5ad16e7a904f4142e2ef3279693945eda
C25177 <beloved25177@126.com>
2024-04-21 18:31:12 +0800

    4.21

--- assets/js/forbiden.js
+++ assets/js/forbiden.js
@@ -1,6 +1,17 @@
 document.oncontextmenu = function () {
-  showNotification("为了有更好的体验，本站禁用右键", 1000);
-  return false;
+  var uA = navigator.userAgent.toLowerCase();
+  var ipad = uA.match(/ipad/i) == "ipad";
+  var iphone = uA.match(/iphone os/i) == "iphone os";
+  var midp = uA.match(/midp/i) == "midp";
+  var uc7 = uA.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+  var uc = uA.match(/ucweb/i) == "ucweb";
+  var android = uA.match(/android/i) == "android";
+  var windowsce = uA.match(/windows ce/i) == "windows ce";
+  var windowsmd = uA.match(/windows mobile/i) == "windows mobile";
+  if (!(ipad || iphone || midp || uc7 || uc || android || windowsce || windowsmd)) {
+    showNotification("为了有更好的体验，本站禁用右键", 1000);
+  }
+  // return false;
 };
 
 function showNotification(message, duration) {
